7554898c487258bad90fca2d670a40639c264e4d
Alex Zhdanov <qwert2603@mail.ru>
2015-05-12 19:19:10 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 m o n e y
 */
 
+// dispite of so many 'for' it spent less than 1 sec. to find solution
+
 int main() {
 
 	unsigned more;
@@ -21,19 +23,19 @@ int main() {
 	vector<tuple<unsigned, unsigned, unsigned>> results;
 
 	unsigned m, o, r, e, s, n, d, y;
-	m = 1;
-	for (s = 8; s != 10; ++s) {
+	m = 1;	// for optimization
+	for (s = 8; s != 10; ++s) {	// for optimization
 		for (d = 0; d != 10; ++d) {
 			for (y = 0; y != 10; ++y) {
 				for (e = 0; e != 10; ++e) {
-					if ((e + d) % 10 == y) {
+					if ((e + d) % 10 == y) {	// for optimization
 						for (r = 0; r != 10; ++r) {
 							for (n = 0; n != 10; ++n) {
-								if ((r + n + ((e + d) / 10)) % 10 == e) {
+								if ((r + n + ((e + d) / 10)) % 10 == e) {	// for optimization
 									for (o = 0; o != 10; ++o) {
-										if (o + e + ((r + n + ((e + d) / 10)) / 10) == n % 10) {
+										if (o + e + ((r + n + ((e + d) / 10)) / 10) == n % 10) {	// for optimization
 											set<unsigned> uniq{ m, o, r, e, s, n, d, y };
-											if (uniq.size() == 8) {
+											if (uniq.size() == 8) {	// digits should be different
 												more = m * 1000 + o * 100 + r * 10 + e;
 												send = s * 1000 + e * 100 + n * 10 + d;
 												money = m * 10000 + o * 1000 + n * 100 + e * 10 + y;
